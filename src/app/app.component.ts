95bdb0f660d330ff18bc68dc318be5c8e9f8db3b
Datunashvili Giorgi <datunashvili_giorgi@yahoo.com>
2020-12-30 01:20:20 +0400

    ApiPaginationAndUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './shared/models/pagination.model';
+import { IProduct } from './shared/models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TessaWebSPA';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,105 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IBrand } from '../shared/models/brands.model';
+import { IProduct } from '../shared/models/product.model';
+import { ShopParams } from '../shared/models/shopParams';
+import { IType } from '../shared/models/types.model';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', { static: true }) searchTerm: ElementRef;
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  sortOptions = [
+    { name: 'ანბანური', value: 'name' },
+    { name: 'ფასი: დაბლიდან მაღლა', value: 'priceAsc' },
+    { name: 'ფასი: მაღლიდან დაბლა', value: 'priceDesc' },
+  ];
+  shopParams = new ShopParams();
+  totalCount: number;
+
+  constructor(private _shopService: ShopService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this._shopService.getProducts(this.shopParams).subscribe(
+      (response) => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getBrands() {
+    this._shopService.getBrands().subscribe(
+      (response) => {
+        this.brands = [{ id: 0, name: 'ყველა' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes() {
+    this._shopService.getTypes().subscribe(
+      (response) => {
+        this.types = [{ id: 0, name: 'ყველა' }, ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onBrandSelected(brandId: number) {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string) {
+    this.shopParams.sort = sort;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
